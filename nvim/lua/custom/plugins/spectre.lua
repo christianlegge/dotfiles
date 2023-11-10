8925ca6078e1d70452173dc686311c6b3c398b43
Christian Legge <christian@christianlegge.dev>
2023-11-09 23:59:06 -0500

    spectre nvim

---
+++ nvim/lua/custom/plugins/spectre.lua
@@ -0,0 +1,12 @@
+return {
+  'nvim-pack/nvim-spectre',
+  config = function()
+    vim.keymap.set('n', '<leader>sp', '<cmd>lua require("spectre").toggle()<CR>', { desc = 'Search Project (Spectre)' })
+    vim.keymap.set('n', '<leader>sw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
+      desc = 'Search current word',
+    })
+    vim.keymap.set('v', '<leader>sw', '<esc><cmd>lua require("spectre").open_visual()<CR>', {
+      desc = 'Search current word',
+    })
+  end,
+}
