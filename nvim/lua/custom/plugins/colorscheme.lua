243303b1194a336a31a3a483e8de7106624d7b2e
Christian Legge <christian@christianlegge.dev>
2023-11-11 19:02:11 -0500

    [neovim] gruvbox-material scheme

--- nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/lua/custom/plugins/colorscheme.lua
@@ -1,29 +1,53 @@
+local flashLabelColors = {
+	bg = "#ffdddd",
+	fg = "#000000",
+}
+local scheme = "gruvbox-material"
+local initScheme = function()
+	vim.cmd.colorscheme(scheme)
+end
+
 return {
+	{ "sainnhe/gruvbox-material" },
+	{
+		"Mofiqul/dracula.nvim",
+		name = "dracula",
+		init = initScheme,
+
+		opts = { overrides = { FlashLabel = flashLabelColors } },
+	},
+	{
+		"Shatur/neovim-ayu",
+		name = "ayu",
+		init = initScheme,
+		opts = { overrides = { FlashLabel = flashLabelColors } },
+	},
 	{
 		"rose-pine/neovim",
 		name = "rose-pine",
-		init = function()
-			vim.cmd.colorscheme("rose-pine")
-		end,
+		init = initScheme,
 		opts = {
 			highlight_groups = {
-				FlashLabel = {
-					bg = "#ffdddd",
-					fg = "#000000",
-				},
+				FlashLabel = flashLabelColors,
 			},
 		},
 	},
 	{
 		"folke/tokyonight.nvim",
+		init = initScheme,
 		opts = {
 			style = "night",
 			on_highlights = function(hl, c)
-				hl.FlashLabel = {
-					bg = "#ffdddd",
-					fg = "#000000",
-				}
+				hl.FlashLabel = flashLabelColors
 			end,
+		},
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		opts = {
+			options = {
+				theme = scheme,
+			},
 		},
 	},
 }
