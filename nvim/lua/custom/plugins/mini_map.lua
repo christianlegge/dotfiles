462712837a4f0b790a75e944f6a34f9953b95a57
Christian Legge <christian@christianlegge.dev>
2024-04-10 00:42:26 -0400

    [neovim] remove mini.map

--- nvim/lua/custom/plugins/mini_map.lua
+++ nvim/lua/custom/plugins/mini_map.lua
@@ -1,23 +1,24 @@
-return {
-  'echasnovski/mini.map',
-  version = false,
-  config = function()
-    require('mini.map').setup {
-      symbols = {
-        encode = require('mini.map').gen_encode_symbols.dot '4x2',
-        scroll_line = '',
-      },
-      window = { show_integration_count = false },
-      integrations = {
-        require('mini.map').gen_integration.builtin_search(),
-        require('mini.map').gen_integration.diagnostic(),
-        require('mini.map').gen_integration.gitsigns(),
-      },
-    }
-    require('mini.map').toggle()
-  end,
-  -- version = false,
-  -- config = function()
-  --   require('mini.map').setup()
-  -- end,
-}
+return {}
+-- return {
+--   'echasnovski/mini.map',
+--   version = false,
+--   config = function()
+--     require('mini.map').setup {
+--       symbols = {
+--         encode = require('mini.map').gen_encode_symbols.dot '4x2',
+--         scroll_line = '',
+--       },
+--       window = { show_integration_count = false },
+--       integrations = {
+--         require('mini.map').gen_integration.builtin_search(),
+--         require('mini.map').gen_integration.diagnostic(),
+--         require('mini.map').gen_integration.gitsigns(),
+--       },
+--     }
+--     require('mini.map').toggle()
+--   end,
+--   -- version = false,
+--   -- config = function()
+--   --   require('mini.map').setup()
+--   -- end,
+-- }
