fb6fb051851cef33ddf70b366338acd153a9b9df
Christian Legge <christian@christianlegge.dev>
2025-10-21 16:50:48 -0400

    [neovim] update, remove rust-tools

--- nvim/lua/custom/plugins/rust_tools.lua
+++ nvim/lua/custom/plugins/rust_tools.lua
@@ -1,24 +1,25 @@
-return {
-	"simrat39/rust-tools.nvim",
-	opts = {
-		server = {
-			on_attach = function(_, bufnr)
-				local rt = require("rust-tools")
-				-- Hover actions
-				vim.keymap.set(
-					"n",
-					"<leader>da",
-					rt.hover_actions.hover_actions,
-					{ buffer = bufnr }
-				)
-				-- Code action groups
-				vim.keymap.set(
-					"n",
-					"<leader>dg",
-					rt.code_action_group.code_action_group,
-					{ buffer = bufnr }
-				)
-			end,
-		},
-	},
-}
+return {}
+-- return {
+-- 	"simrat39/rust-tools.nvim",
+-- 	opts = {
+-- 		server = {
+-- 			on_attach = function(_, bufnr)
+-- 				local rt = require("rust-tools")
+-- 				-- Hover actions
+-- 				vim.keymap.set(
+-- 					"n",
+-- 					"<leader>da",
+-- 					rt.hover_actions.hover_actions,
+-- 					{ buffer = bufnr }
+-- 				)
+-- 				-- Code action groups
+-- 				vim.keymap.set(
+-- 					"n",
+-- 					"<leader>dg",
+-- 					rt.code_action_group.code_action_group,
+-- 					{ buffer = bufnr }
+-- 				)
+-- 			end,
+-- 		},
+-- 	},
+-- }
