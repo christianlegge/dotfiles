6bc9a0c83ac75c7a81da0a1e702a3adef6a44a41
Christian Legge <christian@christianlegge.dev>
2025-10-25 14:30:31 -0400

    [waybar] all partitions on storage module, right click for pcts

--- waybar/modules/storage.sh
+++ waybar/modules/storage.sh
@@ -4,7 +4,8 @@ mount="/"
 warning=20
 critical=10
 
-df -h -P -l "$mount" | awk -v warning=$warning -v critical=$critical '
+percentage() { 
+	df -h -P -l "$1" | awk -v warning=$warning -v critical=$critical '
 /\/.*/ {
   text=$4
   tooltip="Filesystem: "$1"\rSize: "$2"\rUsed: "$3"\rAvail: "$4"\rUse%: "$5"\rMounted on: "$6
@@ -19,6 +20,32 @@ END {
   } else if ((100 - use) < warning) {
     class="warning"
   }
-  print "{\"text\":\""text"\", \"percentage\":"use",\"tooltip\":\""tooltip"\", \"class\":\""class"\"}"
+  # print "{\"text\":\""text"\", \"percentage\":"use",\"tooltip\":\""tooltip"\", \"class\":\""class"\"}"
+  print use
 }
 '
+}
+
+freespace() { 
+	df -h -P -l "$1" | awk -v warning=$warning -v critical=$critical '
+/\/.*/ {
+  text=$4
+  tooltip="Filesystem: "$1"\rSize: "$2"\rUsed: "$3"\rAvail: "$4"\rUse%: "$5"\rMounted on: "$6
+  use=$5
+  exit 0
+}
+END {
+  class=""
+  gsub(/%$/,"",use)
+  if ((100 - use) < critical) {
+    class="critical"
+  } else if ((100 - use) < warning) {
+    class="warning"
+  }
+  # print "{\"text\":\""text"\", \"percentage\":"use",\"tooltip\":\""tooltip"\", \"class\":\""class"\"}"
+  print text
+}
+'
+}
+
+echo "{\"text\": \"$( freespace "/" ) | $( freespace "/c" ) | $( freespace "/d" ) | $( freespace "/e" )\", \"alt\": \"$( percentage "/" )% | $( percentage "/c" )% | $( percentage "/d" )% | $( percentage "/e" )%\"}"
