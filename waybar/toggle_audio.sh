dc8c474fd7fcc4a67b3a9b30dfab7de0c3b38920
Christian Legge <christian@christianlegge.dev>
2025-11-15 11:30:57 -0500

    [waybar] audio widget default to right click mixer

--- waybar/toggle_audio.sh
+++ waybar/toggle_audio.sh
@@ -2,8 +2,8 @@
 tv="alsa_output.pci-0000_01_00.1.hdmi-stereo"
 mixer="alsa_output.usb-Yamaha_Corporation_MG-XU-00.analog-stereo"
 current=$(pactl get-default-sink)
-if [[ "$current" = "$tv" ]]; then
-	pactl set-default-sink $mixer
-elif [[ "$current" = "$mixer" ]]; then
+if [[ "$current" = "$mixer" ]]; then
 	pactl set-default-sink $tv
+else
+	pactl set-default-sink $mixer
 fi
