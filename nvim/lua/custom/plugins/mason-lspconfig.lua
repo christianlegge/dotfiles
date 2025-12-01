9b8cbb4102a16b14d3edf4483c67e7fe01cdd036
Christian Legge <christian@christianlegge.dev>
2025-12-01 13:03:56 -0500

    [neovim] fix lsp config

--- nvim/lua/custom/plugins/mason-lspconfig.lua
+++ nvim/lua/custom/plugins/mason-lspconfig.lua
@@ -1,6 +1,18 @@
 return {
 	"mason-org/mason-lspconfig.nvim",
-	opts = {},
+	opts = {
+		ensure_installed = {
+			"astro",
+			"emmet_language_server",
+			"eslint",
+			"lua_ls",
+			"rust_analyzer",
+			"stylua",
+			"svelte",
+			"tailwindcss",
+			"ts_ls",
+		},
+	},
 	dependencies = {
 		{ "mason-org/mason.nvim", opts = {} },
 		"neovim/nvim-lspconfig",
