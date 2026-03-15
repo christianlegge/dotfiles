fd1d9fb9b00af49b5b028d98f6ee52dc5f1142dd
Christian Legge <christian@christianlegge.dev>
2026-03-15 11:19:48 -0400

    [waybar] fix audio toggle

--- waybar/toggle_audio.sh
+++ waybar/toggle_audio.sh
@@ -2,7 +2,7 @@
 tv="alsa_output.pci-0000_01_00.1.hdmi-stereo"
 mixer="alsa_output.usb-Yamaha_Corporation_MG-XU-00.analog-stereo"
 current=$(pactl get-default-sink)
-if [[ "$current" = "$mixer" ]]; then
+if [[ "$current" != "$tv" ]]; then
 	pactl set-default-sink $tv
 else
 	pactl set-default-sink $mixer
