5b1b73a9a89d89cf61ad6679f01aabf846087cb5
Christian Legge <christian@christianlegge.dev>
2026-05-09 01:18:11 -0400

    [waybar] audio 2x scroll, front

--- waybar/toggle_audio.sh
+++ waybar/toggle_audio.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 tv="alsa_output.pci-0000_01_00.1.hdmi-stereo"
 mixer="alsa_output.usb-Yamaha_Corporation_MG-XU-00.analog-stereo"
+front="alsa_output.usb-Generic_USB_Audio-00.HiFi__Headphones__sink"
 current=$(pactl get-default-sink)
 if [[ "$current" != "$tv" ]]; then
 	pactl set-default-sink $tv
 else
-	pactl set-default-sink $mixer
+	pactl set-default-sink $front
 fi
