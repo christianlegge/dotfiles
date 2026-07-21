9dd3cd69b0d92ed0052f9bb50cf49f8d6cb6aead
Christian Legge <christian@christianlegge.dev>
2026-07-21 09:55:04 -0400

    [hypr] fix springs for 0.56

--- hypr/config/animations.lua
+++ hypr/config/animations.lua
@@ -1,5 +1,5 @@
 hl.curve("bezier", { type = "bezier", points = { { 0.65, 0 }, { 0.35, 1 } } })
-hl.curve("spring", { type = "spring", mass = 1, stiffness = 30, dampening = 6 })
+hl.curve("spring", { type = "spring", mass = 1, stiffness = 300, dampening = 20 })
 
 hl.animation({
 	leaf = "windows",
